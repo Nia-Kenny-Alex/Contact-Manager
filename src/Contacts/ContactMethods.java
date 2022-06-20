8dc182ac3fb66bc0899f6a6c1dc9081978b8b5ae
Nia Watson <nia.d.watson@gmail.com>
2022-06-20 16:53:19 -0500

    Feat. added Menu

--- src/Contacts/ContactMethods.java
+++ src/Contacts/ContactMethods.java
@@ -1,6 +1,15 @@
 package Contacts;
 
 public class ContactMethods {
+    public static void main(String[] args) {
+        //Control Menu
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+    }
 }
 
 
